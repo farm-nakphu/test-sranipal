63aa838ab4aa6f4afbef89682b5c5c53372688b0
Nonthaporn Nakphu <nonthaporn.nak@gmail.com>
2021-04-21 11:16:05 +0900

    Add project

---
+++ EyeTrackingProject3D/Assets/Scripts/FocusObject.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ViveSR.anipal.Eye;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class FocusObject : MonoBehaviour
+{
+    public Material[] CubeMaterials;
+    public Text PositionText;
+    private FocusInfo FocusInfo;
+    private readonly float MaxDistance = 20;
+    private readonly GazeIndex[] GazePriority = new GazeIndex[] { GazeIndex.COMBINE, GazeIndex.LEFT, GazeIndex.RIGHT };
+    private static EyeData_v2 eyeData = new EyeData_v2();
+    private bool eye_callback_registered = false;
+    private void Start()
+    {
+        if (!SRanipal_Eye_Framework.Instance.EnableEye)
+        {
+            enabled = false;
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+            SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
+
+        if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+        {
+            SRanipal_Eye_v2.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = true;
+        }
+        else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+
+        foreach (GazeIndex index in GazePriority)
+        {
+            Ray GazeRay;
+            int focus_object_layer_id = LayerMask.NameToLayer("NoReflection");
+            bool eye_focus;
+            if (eye_callback_registered)
+                eye_focus = SRanipal_Eye_v2.Focus(index, out GazeRay, out FocusInfo, 0, MaxDistance, eyeData);
+            else
+                eye_focus = SRanipal_Eye_v2.Focus(index, out GazeRay, out FocusInfo, 0, MaxDistance);
+
+            if (eye_focus)
+            {
+                FocusInfo.transform.GetComponent<Renderer>().material = CubeMaterials[1];
+                PositionText.text = FocusInfo.point.ToString();
+            }
+            else
+            {
+                GameObject[] cubes = GameObject.FindGameObjectsWithTag("FocusObject");
+
+                foreach (GameObject cube in cubes)
+                {
+                    cube.GetComponent<Renderer>().material = CubeMaterials[0];
+                }
+                PositionText.text = "N/A";
+            }
+        }
+
+        //Debug.Log(eyeData.timestamp.ToString());
+    }
+
+    private void Release()
+    {
+        if (eye_callback_registered == true)
+        {
+            SRanipal_Eye_v2.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye_v2.CallbackBasic)EyeCallback));
+            eye_callback_registered = false;
+        }
+    }
+
+    private static void EyeCallback(ref EyeData_v2 eye_data)
+    {
+        eyeData = eye_data;
+
+        //Debug.Log(eyeData.timestamp.ToString());
+    }
+
+
+
+}
